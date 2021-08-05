e278868713fc10939d1f15439e6df9d158baac5d
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-05 19:21:40 +0530

    Added:-inserting the position in list

--- src/com/bridgelabz/SingleLinkedList.java
+++ src/com/bridgelabz/SingleLinkedList.java
@@ -69,13 +69,30 @@ public class SingleLinkedList {
             temp.next = node;
         }
     }
+    public Node getHead(){
+        return head;
+    }
 
+    public void addAtPos(int pos, int x, Node head) {
+        Node temp = head;
+        Node temp2 = new Node(x);
+        temp2.next = null;
+        pos--;
+        while (pos != 1){
+            temp = temp.next;
+            pos--;
+        }
+        temp2.next = temp.next;
+        temp.next = temp2;
+    }
     public static void main(String[] args) {
         SingleLinkedList slList = new SingleLinkedList();
         //Add nodes to the list at End
-        slList.addToEnd(70);
-        slList.addToEnd(30);
         slList.addToEnd(56);
+        slList.addToEnd(70);
+
+        //Add nodes to the given pos
+        slList.addAtPos(2, 30, slList.head);
 
 
         //Call the Displays Method
